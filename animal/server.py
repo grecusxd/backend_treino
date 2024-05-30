c82ae950a588734507bc1d0816d3c4ec050a3f14
grecusxd <douglas.gomes41@gmail.com>
2024-05-30 12:43:00 -0300

    primeiro commit

---
+++ animal/server.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+import uvicorn
+from typing import List, Optional
+from pydantic import BaseModel
+from uuid import uuid4
+
+app=FastAPI()
+
+class Animal(BaseModel):
+    id: Optional[int]
+    nome: str
+    idade: int
+    sexo: str
+    cor: str
+
+banco: List[Animal] =[]
+
+@app.get('/animais')
+def listar_animais():
+    return banco
+
+@app.get('/animais/{animal_id}')
+def obter_animal(animal_id: str):
+    for animal in banco:
+        if animal.id == animal_id:
+            return animal
+    return {'erro': "animal não localizado"}
+
+@app.delete('/animais/revomer/{animal_id}')
+def remover_animal(animal_id):
+    posicao=-1
+    for index, animal in enumerate(banco):
+        if animal.id == animal_id:
+            posicao=index
+            break
+    
+    if posicao != -1:
+        banco.pop(posicao)
+        return {'msg': 'Animal removido com suceso'}
+    else:
+        return {'erro': "animal não localizado"}        
+
+@app.post('/animais')
+def criar_animal(animal:Animal):
+    animal.id=str(uuid4())
+    banco.append(animal)
+    return None
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000)
---
+++ server.py
@@ -0,0 +1,33 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+
+@app.get('/saudacao/{nome}')
+def home(nome: str):
+    texto = f'Olá {nome}, tudo bem?'
+    return {"mensagemn":texto}
+
+@app.get("/quadrado/{numero}")
+def quadrado(numero: int):
+    resultado=numero*numero
+    texto=f'O quadrado de d {numero} é {resultado}'
+    return {"mensagem": texto}
+
+@app.get("/dobro")
+def dobro(numero: int):
+    resultado= 2*numero
+    return {"Resultado": f"O dobro de {numero} é {resultado}"}
+
+
+@app.get("/area-retangulo")
+def area_retangulo(largura: int, altura: int =2 ):
+    area = largura*altura
+    
+    return {'area': area }
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000)
